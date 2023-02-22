a2b0665a3f40bc1f57839fa8de52274163fd7a94
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-22 12:13:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace CYKLY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int body = 0;
+            Console.WriteLine("Je Slunce koulí žhavých plynů?");
+            string spravnaodpoved = "ano";
+            string odpoved = Console.ReadLine();
+            if (odpoved.ToLower() == spravnaodpoved.ToLower())
+            {
+                Console.WriteLine("Správně! Předtím jsi měl {0} bodů a teď máš o jeden víc!",++body);
+
+            }
+
+            
+        }
+    }
+}
